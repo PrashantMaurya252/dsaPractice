02d4b83aa834e27407d0d8f9dfe961b19d4fe748
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-08-19 22:30:22 +0530

    recursion

---
+++ Recursion/reverse-a-string.js
@@ -0,0 +1,12 @@
+function reverseString(str){
+    if(str === ""){
+        return ""
+    }
+    console.log(reverseString(str.substring(1))+str.charAt(0),"return")
+    console.log(str.substring(1),"sub-string")
+    return reverseString(str.substring(1))+str.charAt(0)
+}
+
+// console.log(reverseString("hello"))
+
+console.log("Hello".substring(1))
